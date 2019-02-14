c43738a06a2a8b506c7c0195f795a60ced5754d5
Abhishikt Emmanuel Prakash <deepikaabhiprakash02@gmail.com>
2019-02-15 01:51:13 +0530

    Truth table generator

---
+++ truthtable.java
@@ -0,0 +1,24 @@
+import java.util.*                                        ;
+class truthtable                                          {
+    Scanner ob=new Scanner(System.in)                     ;
+    void main()                                           {
+        System.out.println("ENTER END NUMBER")            ;
+        int n=ob.nextInt()                                ;
+        int d=(int)Math.pow(2,n)                          ;
+        int a[][]=new int[d][n]                           ;
+        int x=0                                           ;
+        if(x<=n)
+            for(int r=0;r<d;r++)                          {
+                int c=n-1;x=r                             ;
+                while(c>=0)                               {
+                    a[r][c]=x%2                           ;
+                    c--                                   ;
+                    x=x/2;                                }                               
+                                                          }
+        for(int i=0;i<d;i++)                              {
+            System.out.print(i+"  ")                      ;
+            for(int j=0;j<n;j++)                          {
+                System.out.print(a[i][j]+" ");            }
+            System.out.println();                         }
+                                                          }
+                                                          }
