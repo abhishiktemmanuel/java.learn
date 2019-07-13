b7e140695fb83759dec480fb9322bd578702c009
Abhishikt Emmanuel Prakash <deepikaabhiprakash02@gmail.com>
2019-07-14 01:07:09 +0530

    Program to obtain each pallendrom in a string

--- all_pallendrom11.java
+++ all_pallendrom11.java
@@ -1,6 +1,6 @@
 class all_pallendrom11{
     void main(String s){
-        s=" "+s+" ";
+        s=" "+s+".";
         for(int i=1,k=0;i<s.length()-1;i++,k=0)
             while(s.charAt(i-k)==s.charAt(i+k))
                     System.out.println(s.substring((i-k),(i+(++k))));
