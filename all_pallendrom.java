e9e0ca2f01521b5f62c92e90377504dea35a0a76
Abhishikt Emmanuel Prakash <deepikaabhiprakash02@gmail.com>
2019-07-14 01:06:09 +0530

    Program to obtain each pallendrom in a string

--- all_pallendrom.java
+++ all_pallendrom.java
@@ -3,7 +3,7 @@ class all_pallendrom{
     Scanner ob=new Scanner(System.in);
     void main(){
         System.out.println("ENTER WORD");
-        String s=" "+ob.nextLine()+" ";
+        String s=" "+ob.nextLine()+".";
         for(int i=1,k=0;i<s.length()-1;i++,k=0)
             while(s.charAt(i-k)==s.charAt(i+k))
                 System.out.println(s.substring((i-k),(i+(++k))));
